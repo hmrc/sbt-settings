6685bdb68c13fc64aee80b3b4513396fbe063d12
Duncan Crawford <duncan@tandemconsulting.eu>
2014-06-12 11:41:31 +0100

    increment 1.3.0-SNAPSHOT

--- project/PluginBuild.scala
+++ project/PluginBuild.scala
@@ -21,7 +21,7 @@ object PluginBuild extends Build {
   import net.virtualvoid.sbt.graph.Plugin.graphSettings
 
   val pluginName = "sbt-utils"
-  val pluginVersion = "1.2.0"
+  val pluginVersion = "1.3.0-SNAPSHOT"
 
   lazy val root = Project(pluginName, base = file("."), settings = Project.defaultSettings ++ Seq(
 
