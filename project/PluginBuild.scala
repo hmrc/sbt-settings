7d83e026b54b80cc411a4dec9bb446323bac883b
Duncan Crawford <duncan@tandemconsulting.eu>
2014-06-17 22:18:39 +0100

    wire latest version of sbt-git-stamp
    this has a fix containing NPE when the git repo does not have any tags

--- project/PluginBuild.scala
+++ project/PluginBuild.scala
@@ -34,7 +34,7 @@ object PluginBuild extends Build {
         Opts.resolver.sonatypeReleases,
         Opts.resolver.sonatypeSnapshots
       ),
-      addSbtPlugin("uk.gov.hmrc" % "sbt-git-stamp" % "4.1.0"),
+      addSbtPlugin("uk.gov.hmrc" % "sbt-git-stamp" % "4.2.0"),
       addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.3.2"),
       addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.4"),
       publishArtifact := true,
