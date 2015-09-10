7baa8ef7affe4e9c32612296c0005d4b0cee349a
Steve Smith <steve@alwaysagileconsulting.com>
2015-09-10 11:26:22 +0100

    Update sbt-git-stamp

--- project/PluginBuild.scala
+++ project/PluginBuild.scala
@@ -33,7 +33,7 @@ object PluginBuild extends Build {
       organization := "uk.gov.hmrc",
       scalaVersion := "2.10.4",
       resolvers += Resolver.url("hmrc-sbt-plugin-releases", url("https://dl.bintray.com/hmrc/sbt-plugin-releases"))(Resolver.ivyStylePatterns),
-      addSbtPlugin("uk.gov.hmrc" % "sbt-git-stamp" % "5.1.0"),
+      addSbtPlugin("uk.gov.hmrc" % "sbt-git-stamp" % "5.2.0"),
       addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.3.2"),
       publishArtifact := true,
       publishArtifact in Test := false
